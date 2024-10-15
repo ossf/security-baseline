f2e980cd0db4f326c130f34a56acfea9f8b9f4e2
Eddie Knight <knight@linux.com>
2024-10-15 09:17:05 -0500

    Added IDs and details doc (#27)
    * Added IDs and details doc
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Added IDs to criteria.md
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Removed details.md
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * some progress on revising implementation statements
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * reducing line length for github PR diff view. reviewing phrasing.
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * auto-formatting wasn't working for yaml line length, removing settings files
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Refined criteria docs
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Updated spacing, added instructional comments
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * trying to get all the spacing set so that it presents well on github diffs
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Fixed note go.mod->go.sum
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Basic compilation to markdown
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Added lexicon to generated md
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Added links to generated md
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Fixed omitted links
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Fixed edge cases for plurals and capitalization
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Fixed duplication of linking
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * revised check for terms that are already linked
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * typofix
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Polishing the generator a bit
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Removed handcrafted markdown files
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * typofix
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    ---------
    
    Signed-off-by: Eddie Knight <knight@linux.com>

---
+++ cmd/main.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"text/template"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
+)
+
+// Struct for representing each criteria entry
+type Criteria struct {
+	ID                    string `yaml:"id"`
+	MaturityLevel         int    `yaml:"maturity_level"`
+	Category              string `yaml:"category"`
+	CriteriaText          string `yaml:"criteria"`
+	Objective             string `yaml:"objective"`
+	Implementation        string `yaml:"implementation"`
+	ControlMappings       string `yaml:"control_mappings"`
+	SecurityInsightsValue string `yaml:"security_insights_value"`
+	ScorecardProbe        string `yaml:"scorecard_probe"`
+}
+
+// Struct for holding the entire YAML structure
+type Baseline struct {
+	Criteria []Criteria     `yaml:"criteria"`
+	Lexicon  []LexiconEntry `yaml:"lexicon"`
+}
+
+type LexiconEntry struct {
+	Term       string   `yaml:"term"`
+	Definition string   `yaml:"definition"`
+	Synonyms   []string `yaml:"synonyms"`
+}
+
+// Root command for Cobra
+var (
+	Data         Baseline
+	templatePath = "template.md"
+	YAMLpath     string
+	OutputPath   string
+
+	rootCmd = &cobra.Command{
+		Use:  "baseline-compiler",
+		Long: `Baseline Compiler reads the Basline YAML and outputs it as a markdown document.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+	compileCmd = &cobra.Command{
+		Use:   "compile [file]",
+		Short: "Compile a YAML file of security criteria",
+		Args:  cobra.MinimumNArgs(0),
+		Run: func(cmd *cobra.Command, args []string) {
+			err := readYAMLFile()
+			if err != nil {
+				log.Fatalf("Error reading YAML file: %v", err)
+			}
+			err = generateBaselineMdFile()
+			if err != nil {
+				log.Fatalf("Error generating output: %v", err)
+			}
+			fmt.Println("---")
+			fmt.Printf("Output generated to %s\n", filepath.Join("..", "baseline.md"))
+			fmt.Println("Please verify the contents before committing.")
+			fmt.Println("Known issues exist with links where one term is a substring of another, such as 'release' and 'release pipeline'")
+			fmt.Println("---")
+		},
+	}
+)
+
+func init() {
+	compileCmd.Flags().StringVarP(&OutputPath, "output", "o", filepath.Join("..", "baseline.md"), "Output file path")
+	viper.BindPFlag("output", compileCmd.Flags().Lookup("output"))
+
+	compileCmd.Flags().StringVarP(&YAMLpath, "file", "f", filepath.Join("..", "baseline.yaml"), "Path to the YAML input file")
+	viper.BindPFlag("file", compileCmd.Flags().Lookup("file"))
+}
+
+func main() {
+	// Add compile command to root command
+	rootCmd.AddCommand(compileCmd)
+
+	// Execute Cobra root command
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// Function to read the YAML file
+func readYAMLFile() error {
+	file, err := os.Open(YAMLpath)
+	if err != nil {
+		return fmt.Errorf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	var baseline Baseline
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(&baseline); err != nil {
+		return fmt.Errorf("error decoding YAML: %v", err)
+	}
+	Data = baseline
+	return nil
+}
+
+func containsSynonym(list []string, entryTerm, term string) bool {
+	if strings.EqualFold(entryTerm, term) {
+		return true
+	}
+	for _, item := range list {
+		if strings.EqualFold(item, term) {
+			return true
+		}
+	}
+	return false
+}
+
+// Check whether there's an unmatched open bracket before the term
+func isWrapped(text string, matched string) bool {
+	beforeIndex := strings.Index(text, matched)
+	substrBeforeTerm := text[:beforeIndex]
+
+	openBrackets := strings.Count(substrBeforeTerm, "[")
+	closeBrackets := strings.Count(substrBeforeTerm, "]")
+
+	return openBrackets > closeBrackets
+}
+
+// Function to add links by wrapping terms with square brackets
+func addLinks(text, term string) string {
+	// Escape any special characters in the term to use in regex
+	escapedTerm := regexp.QuoteMeta(term)
+
+	// Create a regular expression to match the term as a whole word
+	// The `(?i)` part makes it case-insensitive, and `\b` ensures whole word matching
+	termRegex := regexp.MustCompile(`(?i)\b` + escapedTerm + `(?:s)?\b`)
+
+	// Replace the term with the same term wrapped in brackets, and avoid rewrapping terms
+	return termRegex.ReplaceAllStringFunc(text, func(matched string) string {
+		if isWrapped(text, matched) {
+			return matched // Skip wrapping if already wrapped in brackets
+		}
+
+		for i, entry := range Data.Lexicon {
+			if entry.Term == term && !containsSynonym(entry.Synonyms, entry.Term, matched) {
+				Data.Lexicon[i].Synonyms = append(entry.Synonyms, matched)
+			}
+		}
+		return fmt.Sprintf("[%s]", matched)
+	})
+}
+
+// Main function to apply the term replacements
+func addLinksTemplateFunction(text string) string {
+	// Iterate over the lexicon and replace terms with brackets
+	for _, entry := range Data.Lexicon {
+		text = addLinks(text, entry.Term)
+		for _, synonym := range entry.Synonyms {
+			text = addLinks(text, synonym)
+		}
+	}
+
+	return text
+}
+
+func asLinkTemplateFunction(text string) string {
+	return "#" + strings.ToLower(strings.ReplaceAll(text, " ", "-"))
+}
+
+// Function to generate the markdown file
+func generateBaselineMdFile() (err error) {
+	// Open or create the output file
+	outputFile, err := os.Create(OutputPath)
+	if err != nil {
+		return fmt.Errorf("error creating output file %s: %w", OutputPath, err)
+	}
+	defer outputFile.Close()
+
+	// Read the markdown template from the external file
+	templateContent, err := os.ReadFile(templatePath)
+	if err != nil {
+		return fmt.Errorf("error reading template file: %w", err)
+	}
+
+	// Create and parse the template
+	tmpl, err := template.New("baseline").Funcs(template.FuncMap{
+		// Template function to remove newlines and collapse text
+		"collapseNewlines": func(s string) string {
+			return strings.ReplaceAll(s, "\n", " ")
+		},
+		"addLinks": func(s string) string {
+			return addLinksTemplateFunction(s)
+		},
+		"asLink": func(s string) string {
+			return asLinkTemplateFunction(s)
+		},
+	}).Parse(string(templateContent))
+	if err != nil {
+		return fmt.Errorf("error parsing template: %w", err)
+	}
+
+	// Execute the template and write to the output file
+	err = tmpl.Execute(outputFile, Data)
+	if err != nil {
+		return fmt.Errorf("error executing template: %w", err)
+	}
+
+	return nil
+}
---
+++ cmd/template.md
@@ -0,0 +1,83 @@
+# Open Source Project Security Baseline
+
+## Overview
+
+The Basleine is a set of security criteria that projects should meet to be considered secure. The criteria are organized by maturity level and category. In the detailed subsections you will find the criteria, objectives, and implementation notes.
+
+> [!NOTE]
+> This document was automatically generated from the [Open Source Project Security Baseline YAML](https://github.com/ossf/security-baselines/blob/main/baselines/ossf-security-baseline.yaml) file.
+
+---
+
+## Criteria Overview
+
+| ID  | Maturity Level | Category | Criteria |
+| --- | -------------- | -------- | -------- |
+
+{{- range .Criteria }}
+| [{{ .ID }}]({{ .ID | asLink }}) | {{ .MaturityLevel }} | {{ .Category }} | {{ .CriteriaText | collapseNewlines | addLinks }} |
+{{- end }}
+
+## Criteria Details
+
+{{- range .Criteria }}
+
+### {{ .ID }}
+
+**Criteria:**
+
+{{ .CriteriaText | addLinks }}
+**Objective:**
+
+{{ .Objective | addLinks}}
+**Implementation:**
+
+{{ .Implementation | addLinks }}
+**Control Mappings:**  
+{{ if .ControlMappings }}  
+{{ .ControlMappings }}  
+{{ else }}  
+_No control mappings identified._  
+{{- end }}
+
+**Security Insights Value:**  
+{{ if .SecurityInsightsValue }}  
+{{ .SecurityInsightsValue }}  
+{{ else }}  
+_No security insights identified._  
+{{- end }}
+
+**Scorecard Probe:**  
+{{ if .ScorecardProbe }}  
+{{ .ScorecardProbe }}  
+{{ else }}  
+_No scorecard probe identified._  
+{{- end }}
+{{- end }}
+## Lexicon
+
+{{- range .Lexicon }}
+### {{ .Term }}
+
+{{ .Definition }}
+
+{{- end }}
+---
+
+## Acknowledgments
+
+This baseline was created by community leaders from across the Linux Foundation, including:
+
+- OpenJS Foundation (OpenJS)
+- Open Source Security Foundation (OpenSSF)
+- Cloud Native Computing Foundation (CNCF)
+- Fintech Open Source Foundation (FINOS)
+{{ range .Lexicon }}
+[{{ .Term }}]: {{ .Term | asLink }}
+{{- if .Synonyms }}
+{{- $term := .Term }}
+{{- range .Synonyms }}
+[{{.}}]: {{ $term | asLink }}
+{{- end }}
+{{- end }}
+{{- end }}
