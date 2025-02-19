e21f2e3ad4b2b2767a216de0a2233513889566f2
Puerco <puerco@users.noreply.github.com>
2025-02-18 22:22:13 -0500

    Add data validator, validate subcommand (#188)
    * Add validate subcommand
    
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
    
    * Prevent rendering with invalid data data
    
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
    
    ---------
    
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	if err := cmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
 		os.Exit(1)
 	}
 }
